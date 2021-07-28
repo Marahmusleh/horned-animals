361bab1697a3a5f1fe84a5044ec2d57c54d0b227
marahmusleh <marahaltarefe18@gmail.com>
2021-07-28 23:24:26 +0300

    add a form

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import FloatingLabel from "react-bootstrap/FloatingLabel";
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 
 class Main extends React.Component {
