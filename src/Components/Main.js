cc56dc65231c14d6ab62790abc34f9c533864f60
marahmusleh <marahaltarefe18@gmail.com>
2021-07-28 23:16:12 +0300

    add a form

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,26 +1,72 @@
 import React from 'react';
-
 import HornedBeasts from './HornedBeasts';
-import { Col } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      horned: this.props.Data,
+    };
+  }
+  handleChange = (e) => {
+    console.log(e.target.value);
+    if (e.target.value !== 'all') {
+      const compareHornes = this.props.Data.filter(
+        (item) => item.horns === parseInt(e.target.value)
+      );
+      this.setState({
+        horned: compareHornes,
+      });
+      console.log(compareHornes);
+    } else {
+      this.setState({
+        horned: this.props.Data,
+      });
+    }
+
+    // console.log(e.target.value)
+  };
+
   render() {
     return (
-      <main>
-        {this.props.Data.map((element) => {
-          return (
-            <Col>
-              <HornedBeasts
-                title={element.title}
-                description={element.description}
-                image_url={element.image_url}
-                dataHandling={this.props.showModel}
-                displayModal={this.props.displayModal}
-              />
-            </Col>
-          );
-        })}
-      </main>
+      <div>
+        <Container className='p-3'>
+          <Form.Control
+            as='select'
+            defaultValue='all'
+            onChange={(e) => this.handleChange(e)}
+          >
+            <option value='all'> All</option>
+            <option value='1'> One</option>
+            <option value='2'> Two</option>
+            <option value='3'> Three</option>
+            <option value='100'> One Hunderd</option>
+          </Form.Control>
+        </Container>
+
+        <Container>
+          <Row xs={1} md={3} className='g-4'>
+            {this.state.horned.map((value, i) => {
+              return (
+                <Col key={i}>
+                  <HornedBeasts
+                    title={value.title}
+                    image_url={value.image_url}
+                    keyword={value.keyword}
+                    description={value.description}
+                    horns={value.horns}
+                    displayModal={this.props.displayModal}
+                  />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
